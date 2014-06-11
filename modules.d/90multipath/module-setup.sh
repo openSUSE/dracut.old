2313e90c452c57efbde299fa2a921bed64e43138
Hannes Reinecke <hare@suse.de>
2014-06-11 10:16:46 +0200

    90multipath: Install libgcc_s library
    multipathd is using pthreads, which require libgcc_s for
    pthread_cancel to work. Without it multipathd might crash
    with SIGABRT.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/90multipath/module-setup.sh
+++ modules.d/90multipath/module-setup.sh
@@ -89,6 +89,7 @@ install() {
     inst $(command -v partx) /sbin/partx
 
     inst_libdir_file "libmultipath*" "multipath/*"
+    inst_libdir_file 'libgcc_s.so*'
 
     if dracut_module_included "systemd"; then
         inst_simple "${moddir}/multipathd.service" "${systemdsystemunitdir}/multipathd.service"
