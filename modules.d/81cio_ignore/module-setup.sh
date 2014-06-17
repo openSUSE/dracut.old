7928398bfcc217011a5ec289b1f8b1a74406b906
Hannes Reinecke <hare@suse.de>
2014-06-17 16:45:06 +0200

    81cio_ignore: rewrite module
    Rewrite cio_ignore module to rely on the dracut commandline
    parameter 'rd.cio_accept', which takes a comma-separated list
    of CCW IDs. Each of those IDs are being removed from the
    list of devices from cio_ignore.
    
    The default values for rd.cio_accept are taken from
    /boot/zipl/active_devices.txt.
    
    References: bnc#882685
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/81cio_ignore/module-setup.sh
+++ modules.d/81cio_ignore/module-setup.sh
@@ -10,39 +10,30 @@ check() {
     return 0
 }
 
-find_mount() {
-    local dev mnt etc wanted_dev zipl_dev
-    wanted_dev="$(readlink -e -q $1)"
-    while read dev mnt etc; do
-        [ "$mnt" = "$wanted_dev" ] && zipl_dev="$dev" && break
-    done < /etc/fstab
-    if [ -z "$zipl_dev" ] ; then
-        return 1
-    fi
-    if [ -e ${wanted_dev}/active_devices.txt ] ; then
-        echo "$zipl_dev"
-        return 0
-    fi
-    return 1
-}
-
 cmdline() {
-    local zipl_dasd
-    zipl_dasd=`find_mount /boot/zipl`
-    if [ -n "$zipl_dasd" ] ; then
-	printf " rd.zipl_dasd=%s " $zipl_dasd
+    local cio_accept
+
+    if [ -e /boot/zipl/active_devices.txt ] ; then
+        while read dev etc ; do
+            [ "$dev" = "#" -o "$dev" = "" ] && continue;
+            if [ -z "$cio_accept" ] ; then
+                cio_accept="$dev"
+            else
+                cio_accept="${cio_accept},${dev}"
+            fi
+        done < /boot/zipl/active_devices.txt
+    fi
+    if [ -n "$cio_accept" ] ; then
+	echo "rd.cio_accept=${cio_accept}"
     fi
 }
 
 # called by dracut
 install() {
     if [[ $hostonly_cmdline == "yes" ]];then
-	echo $(cmdline) >"${initdir}/etc/cmdline.d/01zipl_dasd.conf"
+	cmdline >> "${initdir}/etc/cmdline.d/01cio_accept.conf"
     fi
 
-    inst_hook pre-mount 10 "$moddir/parse-zipl.sh"
-    inst_multiple cio_ignore mount umount mkdir
-}
-installkernel() {
-    instmods ext4
+    inst_hook cmdline 20 "$moddir/parse-cio_accept.sh"
+    inst_multiple cio_ignore
 }
