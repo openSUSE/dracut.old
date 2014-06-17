22890ea2c26398207dc155d14f7e0d912ecc9478
Hannes Reinecke <hare@suse.de>
2014-06-17 11:09:59 +0200

    81cio_ignore: skip module if cio_ignore is not active
    When cio_ignore is not active we should skip the entire module
    during boot; otherwise it'll lead to adverse effects.
    
    References: bnc#882685
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/81cio_ignore/parse-zipl.sh
+++ modules.d/81cio_ignore/parse-zipl.sh
@@ -2,17 +2,20 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-ZIPL_DEV="$(getarg rd.zipl_dasd)"
-ZIPL_DIR=/tmp/zipl
+ZIPL_DEV=$(getarg rd.zipl_dasd)
+ZIPL_DIR=/boot/zipl
 CIO_REMOVE_LIST=$ZIPL_DIR/active_devices.txt
 
-if [ -n $ZIPL_DEV ];then
+if ! getarg cio_ignore ; then
+    return
+fi
+if [ -n "$ZIPL_DEV" ];then
 	info "Waiting for zipl device $ZIPL_DEV"
 	wait_for_dev -n "$ZIPL_DEV"
 #
 #	mount device and read devices
 #
-	[ -d $ZIPL_DIR ] ||  mkdir $ZIPL_DIR
+	[ -d $ZIPL_DIR ] ||  mkdir -p $ZIPL_DIR
 	mount -t ext2 -o ro $ZIPL_DEV $ZIPL_DIR
 	if [ -f $CIO_REMOVE_LIST ] ; then
 #
