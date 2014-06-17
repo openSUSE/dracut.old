8db404e527bae7a478d3b850de2119c2c55ebe09
Hannes Reinecke <hare@suse.de>
2014-06-17 11:09:59 +0200

    81cio_ignore: skip module if cio_ignore is not active
    When cio_ignore is not active we should skip the entire module
    during boot; otherwise it'll lead to adverse effects.
    
    References: bnc#882685
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/81cio_ignore/module-setup.sh
+++ modules.d/81cio_ignore/module-setup.sh
@@ -5,35 +5,44 @@
 # called by dracut
 check() {
 # do not add this module by default
-	local arch=$(uname -m)
-	[ "$arch" = "s390" -o "$arch" = "s390x" ] || return 1
-	return 0
+    local arch=$(uname -m)
+    [ "$arch" = "s390" -o "$arch" = "s390x" ] || return 1
+    return 0
 }
 
 find_mount() {
-	local dev mnt etc wanted_dev
-	wanted_dev="$(readlink -e -q $1)"
-	while read dev mnt etc; do
-		[ "$mnt" = "$wanted_dev" ] && echo "$dev" && return 0
-	done < /proc/mounts
-	return 1
+    local dev mnt etc wanted_dev zipl_dev
+    wanted_dev="$(readlink -e -q $1)"
+    while read dev mnt etc; do
+        [ "$mnt" = "$wanted_dev" ] && zipl_dev="$dev" && break
+    done < /etc/fstab
+    if [ -z "$zipl_dev" ] ; then
+        return 1
+    fi
+    if [ -e ${wanted_dev}/active_devices.txt ] ; then
+        echo "$zipl_dev"
+        return 0
+    fi
+    return 1
 }
 
 cmdline() {
-	local zipl_dasd
-	zipl_dasd=`find_mount /boot/zipl`
-	printf " rd.zipl_dasd=%s\n" "$zipl_dasd "
+    local zipl_dasd
+    zipl_dasd=`find_mount /boot/zipl`
+    if [ -n "$zipl_dasd" ] ; then
+	printf " rd.zipl_dasd=%s " $zipl_dasd
+    fi
 }
 
 # called by dracut
 install() {
-	if [[ $hostonly_cmdline == "yes" ]];then
-		echo $(cmdline) >"${initdir}/etc/cmdline.d/01zipl_dasd.conf"
-	fi
+    if [[ $hostonly_cmdline == "yes" ]];then
+	echo $(cmdline) >"${initdir}/etc/cmdline.d/01zipl_dasd.conf"
+    fi
 
-	inst_hook pre-mount 10 "$moddir/parse-zipl.sh"
-	inst_multiple cio_ignore mount umount mkdir
+    inst_hook pre-mount 10 "$moddir/parse-zipl.sh"
+    inst_multiple cio_ignore mount umount mkdir
 }
 installkernel() {
-	instmods ext4
+    instmods ext4
 }
