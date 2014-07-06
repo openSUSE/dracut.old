8ee03bee338a6472e2ffeaa2646575841b191802
Thorsten Behrens <tbehrens@suse.com>
2014-07-07 01:13:40 +0200

    Fixup missing separators in rootfs-block cmdline
    This fixes up 21e4cbee5d852108880cedc9edc619cdd9b296a5 by
    re-introducing the spaces between the arguments.
    [hare: moving printf statement into one line]
    
    Signed-off-by: Thorsten Behrens <tbehrens@suse.com>
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/95rootfs-block/module-setup.sh
+++ modules.d/95rootfs-block/module-setup.sh
@@ -34,9 +34,9 @@ cmdline_journal() {
 cmdline_rootfs() {
     local dev=/dev/block/$(find_root_block_device)
     if [ -e $dev ]; then
-        printf "root=%s" "$(shorten_persistent_dev "$(get_persistent_dev "$dev")")"
-        printf "rootflags=%s" "$(find_mp_fsopts /)"
-        printf "rootfstype=%s\n" "$(find_mp_fstype /)"
+        printf "root=%s rootflags=%s rootfstype=%s\n" \
+            "$(shorten_persistent_dev "$(get_persistent_dev "$dev")")" \
+            "$(find_mp_fsopts /)" "$(find_mp_fstype /)"
     fi
 }
 
