a402618011051445c6562177163b5a89d700b5ea
Thomas Renninger <trenn@suse.de>
2014-07-08 17:34:08 +0200

    dracut: Enable converting of directory /var/run /var/lock to symlinks to /run
    If /var/run is a directory this module will be added.
    After reboot /var/run and /var/lock will be symlinks.
    Another initrd re-creation will not include convertfs module anymore.
    
    bnc#877680
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>

--- modules.d/30convertfs/do-convertfs.sh
+++ modules.d/30convertfs/do-convertfs.sh
@@ -2,10 +2,27 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+# This converts all, /usr/bin -> /bin, ... and /var/run -> /run
+# Do not enable by default!
 if getargbool 0 rd.convertfs; then
+    info "Converting both /var/run to /run tmpfs and /usr/bin -> /bin"
     if getargbool 0 rd.debug; then
         bash -x convertfs "$NEWROOT" 2>&1 | vinfo
+        exit 0
     else
         convertfs "$NEWROOT" 2>&1 | vinfo
+        exit 0
+    fi
+fi
+
+# This only converts /var/run -> /run as tmpfs
+if ! test -L "$NEWROOT"/var/run;then
+    info "Converting /var/run to /run tmpfs"
+    if getargbool 0 rd.debug; then
+        bash -x convertrunfs "$NEWROOT" 2>&1 | vinfo
+        exit 0
+    else
+        convertrunfs "$NEWROOT" 2>&1 | vinfo
+        exit 0
     fi
 fi
