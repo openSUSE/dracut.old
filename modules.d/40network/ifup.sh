juwolf/dracut

40network: fixup static network configuration
The static network configuration was broken; the 'gateway' was
written as the complete 'ip' command line, but simply sourced
later on.

The patch fixes the gateway registration that the '.gw' file
holds entire 'ip route' command lines, which just needs to
be evaluated later on.

Signed-off-by: Hannes Reinecke <hare@suse.de>

modules.d/40network/ifup.sh
@@ -242,6 +242,19 @@ do_static() {
     fi
 
     [ -n "$gw" ] && echo ip route add default via $gw dev $netif > /tmp/net.$netif.gw
+
+    for ifroute in /etc/sysconfig/network/ifroute-${netif} /etc/sysconfig/network/routes ; do
+        [ -e ${ifroute} ] || continue
+        # Pull in existing routing configuration
+        read ifr_dest ifr_gw ifr_mask ifr_if < ${ifroute}
+        [ -z "$ifr_dest" -o -z "$ifr_gw" ] && continue
+        if [ "$ifr_if" = "-" ] ; then
+            echo ip route add $ifr_dest via $ifr_gw >> /tmp/net.$netif.gw
+        else
+            echo ip route add $ifr_dest via $ifr_gw dev $ifr_if >> /tmp/net.$netif.gw
+        fi
+    done
+
     [ -n "$hostname" ] && echo "echo $hostname > /proc/sys/kernel/hostname" > /tmp/net.$netif.hostname
 
     return 0

openSUSE/dracut.old

modules.d/40network/ifup.sh
@@ -207,6 +207,19 @@ if strglobin $ip '*:*:*'; then
     fi
 
     [ -n "$gw" ] && echo ip route replace default via $gw dev $netif > /tmp/net.$netif.gw
+
+    for ifroute in /etc/sysconfig/network/ifroute-${netif} /etc/sysconfig/netwrk/routes ; do
+        [ -e ${ifroute} ] || continue
+        # Pull in existing routing configuration
+        read ifr_dest ifr_gw ifr_mask ifr_if < ${ifroute}
+        [ -z "$ifr_dest" -o -z "$ifr_gw" ] && continue
+        if [ "$ifr_if" = "-" ] ; then
+            echo ip route add $ifr_dest via $ifr_gw >> /tmp/net.$netif.gw
+        else
+            echo ip route add $ifr_dest via $ifr_gw dev $ifr_if >> /tmp/net.$netif.gw
+        fi
+    done
+
     [ -n "$hostname" ] && echo "echo $hostname > /proc/sys/kernel/hostname" > /tmp/net.$netif.hostname
 
     return 0