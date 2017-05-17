juwolf/dracut

Implement shortcut 'ip=<ifname>:static' for static configuration
To simplify static configurate we should be pulling in the
existing 'ifcfg' files when running in hostonly mode.
With that we can implement the shortcut 'ip=<ifname>:static'
to use the existing configuration from the ifcfg file.

Signed-off-by: Hannes Reinecke <hare@suse.de>

modules.d/40network/parse-ip-opts.sh
@@ -64,7 +64,13 @@ for p in $(getargs ip=); do
     # Error checking for autoconf in combination with other values
     case $autoconf in
         error) die "Error parsing option 'ip=$p'";;
-        bootp|rarp|both) die "Sorry, ip=$autoconf is currenty unsupported";;
+        bootp|rarp|both) die "Sorry, ip=$autoconf is currently unsupported";;
+        static)
+            if [ ! -e /etc/sysconfig/network/ifcfg-${dev} ] ; then
+                warn "No ifcfg configuration present for interface $dev, skipping"
+                continue
+            fi
+            ;;
         none|off)
             [ -z "$ip" ] && \
             die "For argument 'ip=$p'\nValue '$autoconf' without static configuration does not make sense"

openSUSE/dracut.old

modules.d/40network/parse-ip-opts.sh
@@ -69,7 +69,13 @@ for p in $(getargs ip=); do
     for autoopt in $(str_replace "$autoconf" "," " "); do
         case $autoopt in
             error) die "Error parsing option 'ip=$p'";;
-            bootp|rarp|both) die "Sorry, ip=$autoopt is currenty unsupported";;
+            bootp|rarp|both) die "Sorry, ip=$autoopt is currently unsupported";;
+            static)
+		if [ ! -e /etc/sysconfig/network/ifcfg-${dev} ] ; then
+                    warn "No ifcfg configuration present for interface $dev, skipping"
+                    continue
+		fi
+            ;;
             none|off)
                 [ -z "$ip" ] && \
                     die "For argument 'ip=$p'\nValue '$autoopt' without static configuration does not make sense"