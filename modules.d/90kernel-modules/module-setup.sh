danimo/dracut

90kernel-modules: Add PCI host controller modules
Currently there is no usb support on RPi4 in the
initrd phase as the pcie-brcmstb module is missing.
If part of the boot is handled from a USB stick
(e.g. with Ignition), the stick cannot be accessed.

Reference: boo#1162669

modules.d/90kernel-modules/module-setup.sh
@@ -33,6 +33,7 @@ installkernel() {
             "=drivers/tty/serial" \
             "=drivers/input/serio" \
             "=drivers/input/keyboard" \
+            "=drivers/usb/controller" \
             "=drivers/usb/storage" \
             "=drivers/pci/host" \
             ${NULL}

openSUSE/dracut.old

modules.d/90kernel-modules/module-setup.sh
@@ -35,6 +35,7 @@ installkernel() {
             "=drivers/input/keyboard" \
             "=drivers/usb/storage" \
             "=drivers/pci/host" \
+            "=drivers/pci/controller" \
             ${NULL}
 
         instmods \