0b998beebc008f2dda2eed8a36d59f4a258cb27a
Ondrej Holecek <oholecek@suse.com>
2020-03-20 16:31:46 +0100

    Mark interface setup after dhcp
    When no ip= is set on command line, but some module requires network
    if drops to the default dhcp configuration. Mark setup complete after
    dhcp setup.

--- modules.d/35network-legacy/ifup.sh
+++ modules.d/35network-legacy/ifup.sh
@@ -651,6 +651,12 @@ if [ -z "$NO_AUTO_DHCP" ] && [ ! -e /tmp/net.${netif}.up ]; then
             do_dhcp -4
         fi
     fi
+    if [ $? -eq 0 ] && [ -f /tmp/leaseinfo.${netif}* ]; then
+         > /tmp/net.${netif}.did-setup
+         if [ -e /sys/class/net/${netif}/address ]; then
+             > /tmp/net.$(cat /sys/class/net/${netif}/address).did-setup
+         fi
+    fi
 fi
 
 exit 0
